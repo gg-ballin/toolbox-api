5eb015726a4a30830c1706a87af242b730726d75
unknown <gomezger.a9@gmail.com>
2018-10-09 19:00:38 -0300

    nuevos cambios

--- server.js
+++ server.js
@@ -4,19 +4,15 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
- 
 
 
- 
-
-//Ejemplo: GET http://localhost:8080/items/10
-app.get('/items/:id', function(req, res, next) {
+app.get('/:id', function(req, res, next) {
   var itemId = req.params.id;
-  res.send('Get ' + req.params.id);
+  res.status(200).send(req.params.id.toUpperCase());
 });
  
-//Ejemplo: POST http://localhost:8080/items
-app.post('/items', function(req, res) {
+
+app.post('/', function(req, res) {
    var data = req.body.data;
    res.send('Add ' + data);
 });
